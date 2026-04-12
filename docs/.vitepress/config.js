c19d935160cc3667f7d88f42c65058800ae97ba3
zhouchentao666 <zhouchentao6@outlook.com>
2026-04-12 10:18:33 +0800

    Update config.js

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,22 +1,29 @@
 import { defineConfig } from 'vitepress'
 
 export default defineConfig({
-  title: 'Cai Install',
+  title: 'FluentInstall',
   description: 'Fluent Design 版本的 Steam 游戏解锁工具',
 
   base: '/Fluent-Install/',
 
   head: [
-    ['link', { rel: 'icon', href: '/Fluent-Install/icon.ico' }]
+    ['link', { rel: 'icon', href: '/icon.ico' }]
   ],
 
-  outDir: '../dist',
-
   themeConfig: {
+    logo: '/icon.ico',
     nav: [
       { text: '首页', link: '/' },
       { text: '入门指南', link: '/guide/getting-started' },
-      { text: '常见问题', link: '/faq/' }
+      { text: '常见问题', link: '/faq/' },
+      {
+        text: '社区与交流',
+        items: [
+          { text: 'GitHub Issue', link: 'https://github.com/zhouchentao666/Fluent-Install/issues' },
+          { text: 'Q群', link: 'https://qm.qq.com/q/gtTLap5Jw4' },
+          { text: 'TG群', link: 'https://t.me/+vTrqXKpRJE9kNmVl' }
+        ]
+      }
     ],
 
     sidebar: [
@@ -35,7 +42,7 @@ export default defineConfig({
     ],
 
     footer: {
-      message: '基于 PyQt6-Fluent-Widgets 构建',
+      message: 'FluentInstall',
       copyright: 'Copyright © 2024-present'
     },
 
